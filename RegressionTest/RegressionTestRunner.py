cfca1bbb62a68ec9a134bbad2f180eb2c7bfa094
Christian Sailer <sar@mailraum.de>
2017-03-10 16:21:53 +0000

    Updated win32 basline binary and fixed some glitches in the regression script. Also force the build to be in a separate build folder

--- RegressionTest/RegressionTestRunner.py
+++ RegressionTest/RegressionTestRunner.py
@@ -8,25 +8,26 @@ defaultConfigFile = "regressionconfig.json"
 class RegressionTestRunner():
     def __init__(self, configfile, runfunc):
         self.config = config.RegressionConfig(configfile)
-        self.testBinary = runhelpers.getExecutable(self.config.testbinlocation)
+        self.testBinary = runhelpers.getTestExecutable(self.config.testbinlocation)
+        print("Binary under test is " + self.testBinary)
         self.baseBinary = runhelpers.getExecutable(self.config.basebinlocation)
+        print("Baseline binary is " + self.baseBinary)
         self.runfunc = runfunc
 
     def run(self):
         if not os.path.exists(self.config.rundir):
             os.makedirs(self.config.rundir)
         runner = depthmaprunner.DepthmapRegressionRunner( self.runfunc, self.baseBinary, self.testBinary, self.config.rundir )
-        results = {}
+
+        good = True
         for name, case in self.config.testcases.items():
-            results[name] = runner.runTestCase(name, case.cmd.infile, case.cmd.outfile, case.cmd.mode, case.cmd.simpleMode, case.cmd.modeLines)
-            
-        good = True    
-        for name, result in results.items():
-            if not result[0]:
+            print("Running test case " + name)
+            success, output = runner.runTestCase(name, case.cmd.infile, case.cmd.outfile, case.cmd.mode, case.cmd.simpleMode, case.cmd.modeLines)
+            if not success:
                 good = False
-                print( name + " failed: " + result[1])
+                print ("Failed:\n" + output)
             else:
-                print(name + " ok")
+                print("ok")
         return good        
 
 if __name__ == "__main__":
