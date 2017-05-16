065a54049d3009d9c6841f30f864cd11e8e3f60e
Petros Koutsolampros <orange-vertex@users.noreply.github.com>
2017-05-16 16:42:32 +0100

    Update contributor information

--- depthmapX/AboutDlg.cpp
+++ depthmapX/AboutDlg.cpp
@@ -23,7 +23,7 @@ CAboutDlg::CAboutDlg(QWidget *parent)
 	QString m_version_info;
     m_version_info = QString(tr("Version %1%2")).arg(DEPTHMAPX_VERSION).arg(DEPTHMAPX_MINOR_VERSION);
 	QString m_copyright;
-    m_copyright = QString(tr("(C) 2000-2010 University College London, Alasdair Turner, Eva Friedrich\n(C) 2011-2014 Tasos Varoudis\n(C) 2017 Christian Sailer"));
+    m_copyright = QString(tr("(C) 2000-2010 University College London, Alasdair Turner, Eva Friedrich\n(C) 2011-2014 Tasos Varoudis\n(C) 2017 Christian Sailer, Petros Koutsolampros"));
 	QString m_agreement;
     m_agreement = QString(tr("This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\x0D\x0D\x0A\x0D\x0D\x0AThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\x0D\x0D\x0A\x0D\x0D\x0AYou should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>."));
 
