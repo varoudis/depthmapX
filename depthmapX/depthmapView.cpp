94e4e14dd9a12d8255f8dafb8f29e6a6cae89e1a
Petros Koutsolampros <commits@pklampros.io>
2017-08-01 02:45:00 +0100

    Allow picking a pixel colour by providing the pixel reference instead of making the pixel current within the pointmap

--- depthmapX/depthmapView.cpp
+++ depthmapX/depthmapView.cpp
@@ -1438,7 +1438,7 @@ bool QDepthmapView::DrawPoints(QPainter *pDC, QGraphDoc *pDoc, int spacer, unsig
       Point2f logical = map.getNextPointLocation();
 
       PafColor color;
-      color = map.getPointColor();
+      color = map.getCurrentPointColor();
 
       if (color.alphab() != 0) 
 	  { // alpha == 0 is transparent
@@ -1786,7 +1786,7 @@ void QDepthmapView::OutputEPS( ofstream& stream, QGraphDoc *pDoc )
 
          Point2f logical = map.getNextPointLocation();
 
-         PafColor color = map.getPointColor();
+         PafColor color = map.getCurrentPointColor();
 
          if (color.alphab() != 0) { // alpha == 0 is transparent
 
@@ -2566,7 +2566,7 @@ void QDepthmapView::OutputSVG( ofstream& stream, QGraphDoc *pDoc )
       while ( map.findNextPoint() ) {
 
          Point2f logical = map.getNextPointLocation();
-         PafColor color = map.getPointColor();
+         PafColor color = map.getCurrentPointColor();
          
          if (color.alphab() != 0) { // alpha == 0 is transparent
 
