0ba538a09aee7fb8360c4de8bb4bfde99b4561ef
Petros Koutsolampros <commits@pklampros.io>
2017-08-12 12:21:44 +0300

    Force depthmapView to draw on a persistent pixmap
    This allows previously drawn elements to remain on the screen between different paint events. This commit also actually fixes https://github.com/SpaceGroupUCL/depthmapX/issues/36

--- depthmapX/depthmapView.cpp
+++ depthmapX/depthmapView.cpp
@@ -311,7 +311,7 @@ void QDepthmapView::SetRedrawflag()
 
 void QDepthmapView::paintEvent(QPaintEvent *)
 {
-	QPainter pDC(this);
+    QPainter pDC(pix);
 
 	SetRedrawflag();
 
@@ -474,6 +474,9 @@ void QDepthmapView::paintEvent(QPaintEvent *)
 
    m_drawing = false;
    pDoc->m_meta_graph->releaseLock(this);
+
+   QPainter screenPainter(this);
+   screenPainter.drawPixmap(0,0,width(),height(),*pix);
 }
 
 void QDepthmapView::resizeEvent(QResizeEvent *)
@@ -482,6 +485,7 @@ void QDepthmapView::resizeEvent(QResizeEvent *)
    m_redraw_all = true;
    m_resize_viewport = true;
    pDoc->m_view[QGraphDoc::VIEW_MAP] = this;
+   pix = new QPixmap(width(),height());
 }
 
 void QDepthmapView::BeginDrag(QPoint point)
