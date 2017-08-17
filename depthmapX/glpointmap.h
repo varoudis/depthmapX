98af56fb7f181b674ea4bad417a2b640b7b6125c
Petros Koutsolampros <orange-vertex@users.noreply.github.com>
2017-08-17 15:32:29 +0300

    Renove depth-testing and reverse the order of objects to display transparency

--- depthmapX/glpointmap.h
+++ depthmapX/glpointmap.h
@@ -48,14 +48,14 @@ public:
     void paintGLOverlay(const QMatrix4x4 &m_mProj, const QMatrix4x4 &m_mView, const QMatrix4x4 &m_mModel)
     {
         glLineWidth(4);
-        m_linkFills.paintGL(m_mProj, m_mView, m_mModel);
         m_linkLines.paintGL(m_mProj, m_mView, m_mModel);
+        m_linkFills.paintGL(m_mProj, m_mView, m_mModel);
         glLineWidth(1);
     }
     void paintGL(const QMatrix4x4 &m_mProj, const QMatrix4x4 &m_mView, const QMatrix4x4 &m_mModel)
     {
-        m_grid.paintGL(m_mProj, m_mView, m_mModel);
         m_pointMap.paintGL(m_mProj, m_mView, m_mModel);
+        m_grid.paintGL(m_mProj, m_mView, m_mModel);
     }
     void setGridColour(QRgb gridColour) {
         m_gridColour = gridColour;
