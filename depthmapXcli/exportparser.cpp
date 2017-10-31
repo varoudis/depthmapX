00321a0935cf37ae29d3151ff6075748f442fd21
Petros Koutsolampros <orange-vertex@users.noreply.github.com>
2017-10-31 17:48:18 +0000

    Allow exporting VGA data in CSV

--- depthmapXcli/exportparser.cpp
+++ depthmapXcli/exportparser.cpp
@@ -37,7 +37,10 @@ void ExportParser::parse(int argc, char *argv[])
                 throw CommandLineException("-em can only be used once, modes are mutually exclusive");
             }
             ENFORCE_ARGUMENT("-em", i)
-            if ( strcmp(argv[i], "pointmap-connections-csv") == 0 )
+            if ( strcmp(argv[i], "pointmap-data-csv") == 0 )
+            {
+                _exportMode = ExportMode::POINTMAP_DATA_CSV;
+            } else if ( strcmp(argv[i], "pointmap-connections-csv") == 0 )
             {
                 _exportMode = ExportMode::POINTMAP_CONNECTIONS_CSV;
             }
