f7190be5cefbcd1c060d4fe1ca2a3c66e3b6f173
Petros Koutsolampros <commits@pklampros.io>
2018-03-30 14:51:56 +0100

    Restore function in header

--- salalib/mgraph.h
+++ salalib/mgraph.h
@@ -175,6 +175,8 @@ public:
    bool convertToDrawing(Communicator *comm, std::string layer_name, int typeflag); // 0 signifies convert from data map, else convert from graph
    bool convertToConvex(Communicator *comm, std::string layer_name, bool keeporiginal, int typeflag); // -1 signifies convert from drawing layer, else convert from data map
    bool convertAxialToSegment(Communicator *comm, std::string layer_name, bool keeporiginal, bool pushvalues, double stubremoval);
+   // some compatibility with older version horrors:
+   int convertDataLayersToShapeMap(DataLayers& datalayers, PointMap& pointmap);
    int loadMifMap(Communicator *comm, istream& miffile, istream& midfile);
    bool makeAllLineMap( Communicator *communicator, const Point2f& seed );
    bool makeFewestLineMap( Communicator *communicator, int replace );
