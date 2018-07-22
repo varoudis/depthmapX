2be3cf9040aead2c8910b8e69c6fa44e6f19f81e
Petros Koutsolampros <commits@pklampros.io>
2018-07-22 12:17:34 +0100

    Remove unnecessary friends

--- salalib/connector.h
+++ salalib/connector.h
@@ -33,10 +33,6 @@ struct SegmentRef
    int ref;
    SegmentRef(char d = 0, int r = -1): pad1(0), pad2(0)
    { dir = d; ref = r; }
-   friend bool operator < (SegmentRef a, SegmentRef b);
-   friend bool operator > (SegmentRef a, SegmentRef b);
-   friend bool operator == (SegmentRef a, SegmentRef b);
-   friend bool operator != (SegmentRef a, SegmentRef b);
 };
 // note, the dir is only a direction indicator, the ref should always be unique
 inline bool  operator < (SegmentRef a, SegmentRef b) { return a.ref < b.ref; }
