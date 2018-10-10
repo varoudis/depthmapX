83addc6bff6f67b777e51536d267e61c1608a89c
Christian Sailer <sar@mailraum.de>
2018-10-10 20:42:39 +0100

    Added implementation and tests for column matrix

--- genlib/simplematrix.h
+++ genlib/simplematrix.h
@@ -19,6 +19,12 @@
 #include <stdexcept>
 
 namespace depthmapX {
+
+/**
+ *  Base class for 2 dimensional matrices. This can be used as reference/pointer, but you cannot
+ *  create this directly - you need to create either a row or a column matrix (the difference
+ *  being the memory layout either, contiguous rows, or contiguous columns.
+ */
 template<typename T> class BaseMatrix {
 protected:
     BaseMatrix(size_t rows, size_t columns ){
@@ -42,33 +48,74 @@ public:
         delete[] m_data;
     }
 
+    /**
+     * @brief operator () access operator uses () instead of [] to allow giving two coordinates
+     * @param row row to access
+     * @param column column to access
+     * @return non-const reference to the data
+     */
     virtual T & operator()(size_t row, size_t column) = 0;
+
+    /**
+     * @brief operator () access operator uses () instead of [] to allow giving two coordinates
+     * @param row row to access
+     * @param column column to access
+     * @return const reference to the data
+     */
     virtual T const & operator()(size_t row, size_t column) const = 0;
 
+    /**
+     * @brief begin get a pointer to the data array (complies with std::iterator definitions)
+     * @return pointer to the first element
+     */
     T* begin(){
         return m_data;
     }
 
+    /**
+     * @brief end pointer marking the end of the data array
+     * @return pointer behind the last element of the data array
+     */
     T* end(){
         return m_data + size();
     }
 
+    /**
+     * @brief begin get a pointer to the data array (complies with std::iterator definitions)
+     * @return const pointer to the first element
+     */
     T const * begin() const {
         return m_data;
     }
 
+    /**
+     * @brief end pointer marking the end of the data array
+     * @return const pointer behind the last element of the data array
+     */
     T const * end() const {
         return m_data + size();
     }
 
+    /**
+     * @brief size
+     * @return size of the data array in elements
+     */
     size_t size() const {
         return m_rows * m_columns;
     }
 
+    /**
+     * @brief rows
+     * @return number of rows
+     */
     size_t rows() const {
         return m_rows;
     }
 
+    /**
+     * @brief columns
+     * @return number of columns
+     */
     size_t columns() const {
         return m_columns;
     }
@@ -89,6 +136,11 @@ protected:
     }
 };
 
+
+/**
+ * Row matrix implementation - the data for each row is contiguous in memory, columns jump by the
+ * number of rows.
+ */
 template<typename T> class RowMatrix : public BaseMatrix<T>{
 public:
     RowMatrix(size_t rows, size_t columns) : BaseMatrix<T>(rows, columns){}
@@ -113,5 +165,35 @@ public:
         return m_data[column + row * m_columns];
     }
 };
+
+/**
+ * Column matrix implementation - the data for each column is contiguous in memory, rows jump by the
+ * number of columns.
+ */
+template<typename T> class ColumnMatrix : public BaseMatrix<T>{
+public:
+    ColumnMatrix(size_t rows, size_t columns) : BaseMatrix<T>(rows, columns){}
+    ColumnMatrix(ColumnMatrix const & other) : BaseMatrix(other){}
+    ColumnMatrix(ColumnMatrix && other) : BaseMatrix(std::move(other)){}
+
+    ColumnMatrix<T> & operator= ( ColumnMatrix<T> const & other){
+        ColumnMatrix tmp(other);
+        std::swap(m_data, tmp.m_data);
+        std::swap(m_rows, tmp.m_rows);
+        std::swap(m_columns, tmp.m_columns);
+        return *this;
+    }
+
+    T & operator () (size_t row, size_t column){
+        this->access_check(row, column);
+        return m_data[row + column * m_rows];
+    }
+
+    T const & operator ()(size_t row, size_t column) const {
+        this->access_check(row, column);
+        return m_data[row + column * m_rows];
+    }
+};
+
 
 }
--- genlibTest/testsimplematrix.cpp
+++ genlibTest/testsimplematrix.cpp
@@ -25,22 +25,6 @@ template<typename T> void compareMatrixContent( depthmapX::BaseMatrix<T> const &
     REQUIRE(result == expected);
 }
 
-TEST_CASE("First row matrix test"){
-    depthmapX::RowMatrix<int> matrix(2, 3);
-    matrix(0,0) = 1;
-    matrix(1,2) = -1;
-    matrix(0,1) = 2;
-    matrix(0,2) = 3;
-    matrix(1,0) = -23;
-    matrix(1,1) = 0;
-
-    REQUIRE(matrix(1,2) == -1);
-
-    compareMatrixContent(matrix, std::vector<int>{1, 2, 3, -23, 0, -1});
-}
-
-
-
 TEST_CASE("Row matrix test assignemnt copy and move"){
    depthmapX::RowMatrix<std::string> matrix(2,3);
    matrix(0,0) = "0,0";
@@ -80,6 +64,52 @@ TEST_CASE("Row matrix test exceptions"){
     REQUIRE(matrix(1,2) == -1);
 
     compareMatrixContent(matrix, std::vector<int>{1, 2, 3, -23, 0, -1});
+
+    REQUIRE_THROWS_WITH(matrix(-1, 0), Catch::Contains("row out of range"));
+    REQUIRE_THROWS_WITH(matrix(5, 0), Catch::Contains("row out of range"));
+    REQUIRE_THROWS_WITH(matrix(0, -1), Catch::Contains("column out of range"));
+    REQUIRE_THROWS_WITH(matrix(0, 5), Catch::Contains("column out of range"));
+}
+
+TEST_CASE("Column matrix test assignemnt copy and move"){
+   depthmapX::ColumnMatrix<std::string> matrix(2,3);
+   matrix(0,0) = "0,0";
+   matrix(1,0) = "1,0";
+   matrix(0,1) = "0,1";
+   matrix(1,1) = "1,1";
+   matrix(1,2) = "1,2";
+   matrix(0,2) = "0,2";
+
+   std::vector<std::string> expected{"0,0", "1,0", "0,1", "1,1", "0,2", "1,2"};
+   compareMatrixContent(matrix, expected);
+
+   depthmapX::ColumnMatrix<std::string> copy(matrix);
+   compareMatrixContent(matrix, expected);
+   compareMatrixContent(copy, expected);
+
+   depthmapX::ColumnMatrix<std::string> clone(std::move(copy));
+   compareMatrixContent(clone, expected);
+   REQUIRE(copy.size() == 0);
+
+   copy = clone;
+   compareMatrixContent(copy, expected);
+   REQUIRE(copy.columns() == 3);
+   REQUIRE(copy.rows() == 2);
+   compareMatrixContent(clone, expected);
+}
+
+TEST_CASE("Column matrix test exceptions"){
+    depthmapX::ColumnMatrix<int> matrix(2, 3);
+    matrix(0,0) = 1;
+    matrix(1,2) = -1;
+    matrix(0,1) = 2;
+    matrix(0,2) = 3;
+    matrix(1,0) = -23;
+    matrix(1,1) = 0;
+
+    REQUIRE(matrix(1,2) == -1);
+
+    compareMatrixContent(matrix, std::vector<int>{1, -23, 2, 0, 3, -1});
 
     REQUIRE_THROWS_WITH(matrix(-1, 0), Catch::Contains("row out of range"));
     REQUIRE_THROWS_WITH(matrix(5, 0), Catch::Contains("row out of range"));
