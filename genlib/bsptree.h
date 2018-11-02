7e216de5c9422a10129f1637b019962a588e7f6c
Petros Koutsolampros <commits@pklampros.io>
2018-11-02 20:05:23 +0000

    Fix formatting in genlib using ClangFormat
    With the options:
    IndentWidth: 4
    ColumnLimit: 119
    NamespaceIndentation: All

--- genlib/bsptree.h
+++ genlib/bsptree.h
@@ -17,53 +17,49 @@
 
 #pragma once
 
-#include "p2dpoly.h"
+#include "genlib/p2dpoly.h"
+
 #include <memory>
 
 // Binary Space Partition
 
-struct BSPNode
-{
+struct BSPNode {
 
-private:
-   Line m_line;
-   int m_tag;
+  private:
+    Line m_line;
+    int m_tag;
 
-public:
-   enum { BSPLEFT, BSPRIGHT };
-   std::unique_ptr<BSPNode> m_left;
-   std::unique_ptr<BSPNode> m_right;
-   BSPNode *m_parent;
+  public:
+    enum { BSPLEFT, BSPRIGHT };
+    std::unique_ptr<BSPNode> m_left;
+    std::unique_ptr<BSPNode> m_right;
+    BSPNode *m_parent;
 
-   BSPNode(BSPNode *parent = NULL) : m_left(nullptr), m_right(nullptr)
-   {m_parent = parent; m_tag = -1; }
-   //
-   bool isLeaf() {
-      return m_left == nullptr && m_right == nullptr;
-   }
-   int classify(const Point2f& p) {
-       Point2f v0 = m_line.end() - m_line.start();
-       v0.normalise();
-       Point2f v1 = p - m_line.start();
-       v1.normalise();
-       if (det(v0,v1) >= 0) {
-          return BSPLEFT;
-       }
-       else {
-          return BSPRIGHT;
-       }
-   }
-   const Line& getLine() const { return m_line; }
-   void setLine(const Line& line) { m_line = line; }
-   int getTag() const { return m_tag; }
-   void setTag(const int tag) { m_tag = tag; }
+    BSPNode(BSPNode *parent = NULL) : m_left(nullptr), m_right(nullptr) {
+        m_parent = parent;
+        m_tag = -1;
+    }
+    bool isLeaf() { return m_left == nullptr && m_right == nullptr; }
+    int classify(const Point2f &p) {
+        Point2f v0 = m_line.end() - m_line.start();
+        v0.normalise();
+        Point2f v1 = p - m_line.start();
+        v1.normalise();
+        if (det(v0, v1) >= 0) {
+            return BSPLEFT;
+        } else {
+            return BSPRIGHT;
+        }
+    }
+    const Line &getLine() const { return m_line; }
+    void setLine(const Line &line) { m_line = line; }
+    int getTag() const { return m_tag; }
+    void setTag(const int tag) { m_tag = tag; }
 };
 
 namespace BSPTree {
-void make(Communicator *communicator, time_t atime, const std::vector<TaggedLine> &lines, BSPNode *root);
-int pickMidpointLine(const std::vector<TaggedLine> &lines, BSPNode *par);
-std::pair<std::vector<TaggedLine>, std::vector<TaggedLine> > makeLines(Communicator *communicator,
-                                                                       time_t atime,
-                                                                       const std::vector<TaggedLine>& lines,
-                                                                       BSPNode *base);
-}
+    void make(Communicator *communicator, time_t atime, const std::vector<TaggedLine> &lines, BSPNode *root);
+    int pickMidpointLine(const std::vector<TaggedLine> &lines, BSPNode *par);
+    std::pair<std::vector<TaggedLine>, std::vector<TaggedLine>>
+    makeLines(Communicator *communicator, time_t atime, const std::vector<TaggedLine> &lines, BSPNode *base);
+} // namespace BSPTree
