cee4cab36e1a36302e668f9b6706606a170067a1
Petros Koutsolampros <commits@pklampros.io>
2018-11-08 14:12:47 +0000

    Move chunky functions in the .cpp file

--- salalib/vgamodules/vgavisualglobal.cpp
+++ salalib/vgamodules/vgavisualglobal.cpp
@@ -213,3 +213,27 @@ bool VGAVisualGlobal::run(Communicator *comm, const Options &options, PointMap &
 
     return true;
 }
+
+void VGAVisualGlobal::extractUnseen(Node &node, PixelRefVector &pixels, depthmapX::RowMatrix<int> &miscs,
+                   depthmapX::RowMatrix<PixelRef> &extents) {
+    for (int i = 0; i < 32; i++) {
+        Bin &bin = node.bin(i);
+        for (auto pixVec : bin.m_pixel_vecs) {
+            for (PixelRef pix = pixVec.start(); pix.col(bin.m_dir) <= pixVec.end().col(bin.m_dir);) {
+                int &misc = miscs(pix.y, pix.x);
+                PixelRef &extent = extents(pix.y, pix.x);
+                if (misc == 0) {
+                    pixels.push_back(pix);
+                    misc |= (1 << i);
+                }
+                // 10.2.02 revised --- diagonal was breaking this as it was extent in diagonal or horizontal
+                if (!(bin.m_dir & PixelRef::DIAGONAL)) {
+                    if (extent.col(bin.m_dir) >= pixVec.end().col(bin.m_dir))
+                        break;
+                    extent.col(bin.m_dir) = pixVec.end().col(bin.m_dir);
+                }
+                pix.move(bin.m_dir);
+            }
+        }
+    }
+}
--- salalib/vgamodules/vgavisualglobal.h
+++ salalib/vgamodules/vgavisualglobal.h
@@ -30,26 +30,5 @@ class VGAVisualGlobal : IVGA {
     std::string getAnalysisName() const override { return "Global Visibility Analysis"; }
     bool run(Communicator *comm, const Options &options, PointMap &map, bool simple_version) override;
     void extractUnseen(Node &node, PixelRefVector &pixels, depthmapX::RowMatrix<int> &miscs,
-                       depthmapX::RowMatrix<PixelRef> &extents) {
-        for (int i = 0; i < 32; i++) {
-            Bin &bin = node.bin(i);
-            for (auto pixVec : bin.m_pixel_vecs) {
-                for (PixelRef pix = pixVec.start(); pix.col(bin.m_dir) <= pixVec.end().col(bin.m_dir);) {
-                    int &misc = miscs(pix.y, pix.x);
-                    PixelRef &extent = extents(pix.y, pix.x);
-                    if (misc == 0) {
-                        pixels.push_back(pix);
-                        misc |= (1 << i);
-                    }
-                    // 10.2.02 revised --- diagonal was breaking this as it was extent in diagonal or horizontal
-                    if (!(bin.m_dir & PixelRef::DIAGONAL)) {
-                        if (extent.col(bin.m_dir) >= pixVec.end().col(bin.m_dir))
-                            break;
-                        extent.col(bin.m_dir) = pixVec.end().col(bin.m_dir);
-                    }
-                    pix.move(bin.m_dir);
-                }
-            }
-        }
-    }
+                       depthmapX::RowMatrix<PixelRef> &extents);
 };
