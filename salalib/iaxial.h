cb2e1bb9f688ed3038b5505641a00cbf03a5e350
Petros Koutsolampros <commits@pklampros.io>
2018-11-08 14:16:57 +0000

    Move helpers into the helpers file

--- salalib/iaxial.h
+++ salalib/iaxial.h
@@ -30,38 +30,4 @@ public:
     virtual std::string getAnalysisName() const = 0;
     virtual bool run(Communicator *comm, const Options& options, ShapeGraph &map, bool simple_version) = 0;
     virtual ~IAxial(){}
-
-    // Axial map helper: convert a radius for angular analysis
-
-    static std::string makeFloatRadiusText(double radius)
-    {
-       std::string radius_text;
-       if (radius > 100.0) {
-          radius_text = dXstring::formatString(radius,"%.f");
-       }
-       else if (radius < 0.1) {
-          radius_text = dXstring::formatString(radius,"%.4f");
-       }
-       else {
-          radius_text = dXstring::formatString(radius,"%.2f");
-       }
-       return radius_text;
-    }
-
-    static std::string makeRadiusText(int radius_type, double radius)
-    {
-       std::string radius_text;
-       if (radius != -1) {
-          if (radius_type == Options::RADIUS_STEPS) {
-             radius_text = std::string(" R") + dXstring::formatString(int(radius),"%d") + " step";
-          }
-          else if (radius_type == Options::RADIUS_METRIC) {
-             radius_text = std::string(" R") + makeFloatRadiusText(radius) + " metric";
-          }
-          else { // radius angular
-             radius_text = std::string(" R") + makeFloatRadiusText(radius);
-          }
-       }
-       return radius_text;
-    }
 };
