1dda574186e7246860eb8a9d7e6a86c86fc5cac0
Petros Koutsolampros <commits@pklampros.io>
2019-03-04 22:55:03 +0000

    Use empty() instead of size()

--- salalib/shapemap.cpp
+++ salalib/shapemap.cpp
@@ -2574,7 +2574,7 @@ bool ShapeMap::write( std::ofstream& stream, int version )
 
    // left here for backwards-compatibility
    // TODO: Remove at next iteration of the .graph file format
-   int largest_shape_ref = m_shapes.size() != 0 ? m_shapes.rbegin()->first : -1;
+   int largest_shape_ref = m_shapes.empty() ? -1 : m_shapes.rbegin()->first;
    stream.write((char *) &largest_shape_ref, sizeof(largest_shape_ref));
 
    // write shape data
